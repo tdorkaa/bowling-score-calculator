73e0e783a1a056b929edf057d95295d0ff9cb1f6
tdorkaa <tamasdorkaa@gmail.com>
2018-08-02 16:27:16 +0200

    will evaluate frames for spare (also last roll)

--- src/bowling-score-calculator.js
+++ src/bowling-score-calculator.js
@@ -1,7 +1,20 @@
 function bowlingScoreCalculator(scoreSheet) {
     let result = 0;
-    scoreSheet.forEach((frame) => result = result + frame[0] + frame[1]);
+    scoreSheet.forEach((frame, frameIndex) => result = result + getFrameResult(frame, scoreSheet, frameIndex));
     return result;
+}
+
+function getFrameResult(frame, scoreSheet, frameIndex) {
+    const isSpare = frame[1] === "/";
+    const maxPin = 10;
+    const isLastFrame = frameIndex;
+    if(isLastFrame && isSpare) {
+        return maxPin + frame[2];
+    }
+    if(isSpare) {
+        return maxPin + scoreSheet[frameIndex+1][0];
+    }
+    return frame[0] + frame[1];
 }
 
 module.exports = bowlingScoreCalculator;
--- src/bowling-score-calculator.spec.js
+++ src/bowling-score-calculator.spec.js
@@ -20,4 +20,16 @@ describe('bowlingScoreCalculator', function() {
   it('should return 13 when the first roll is 1, the second is 4, third is 6 and forth is 2 and all the other are 0', function () {
     expect(bowlingScoreCalculator([[1, 4], [6, 2], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(13);
   });
+//write test for spare when after spare is 0
+  it('should return correct result when second roll is spare', function () {
+    expect(bowlingScoreCalculator([[1, "/"], [6, 2], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(24);
+  });
+
+  it('should return correct result when the last roll in the 10th frame is spare', function () {
+    expect(bowlingScoreCalculator([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [5, '/', 5]])).to.equal(15);
+  });
+
+  it('should return correct result when the last roll in the 10th frame is spare', function () {
+    expect(bowlingScoreCalculator([["X"], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])).to.equal(15);
+  });
 });
